1cc60319f6d9f4d67062536b8f9b285cdb5633a0
ptsionis <tsionispanagiotis@gmail.com>
2022-12-02 23:58:53 +0200

    added converter lab

---
+++ Web Services and Transactions/Lab02/ConverterLab2/ConverterLab2/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ConverterLab2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Μετατροπή Συναλλάγματος" Font-Bold="True" Font-Size="Larger"></asp:Label><br /><br />
+            <asp:Label ID="Label2" runat="server" Text="Δώστε νόμισμα προέλευσης: "></asp:Label>&nbsp;
+            <asp:TextBox ID="amountCurr" runat="server"></asp:TextBox>&nbsp;
+            <asp:DropDownList ID="fromCurrList" runat="server" AutoPostBack="True"></asp:DropDownList><br /><br />
+            <asp:Label ID="Label3" runat="server" Text="Δώστε νόμισμα προορισμού: "></asp:Label>&nbsp;
+            <asp:DropDownList ID="toCurrList" runat="server" AutoPostBack="True"></asp:DropDownList><br /><br />
+            <asp:Button ID="convertCurr" runat="server" Text="Μετατροπή" OnClick="convertCurr_Click" /><br /><br />
+            <asp:Label ID="resultCurr" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Web Services and Transactions/Lab02/ConverterLab2/ConverterLab2/WebForm1.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace ConverterLab2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        lt.lb.www.ExchangeRates myWS2 = new lt.lb.www.ExchangeRates();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false)
+            {
+                //Receive the XML node - root <Currencies>
+                XmlNode node = myWS2.getListOfCurrencies();
+
+                //Make a list with all <item> nodes from the above node
+                //<Currencies>
+                //  <item>
+                //      ....
+                //  </item>
+                //</Currencies>
+                XmlNodeList nodes = node.SelectNodes("//item");
+
+                //For each item node, insert in my drop-lists the values from each item XML node
+                foreach (XmlNode nd in nodes)
+                {
+                    fromCurrList.Items.Add(nd["currency"].InnerText + " " + nd["description"].InnerText);
+                    toCurrList.Items.Add(nd["currency"].InnerText + " " + nd["description"].InnerText);
+
+                    fromCurrList.SelectedIndex = 26;
+                    toCurrList.SelectedIndex = 87;
+                }
+            }
+        }
+
+        protected void convertCurr_Click(object sender, EventArgs e)
+        {
+            decimal f, rateFrom = 0, rateTo = 0;
+            string curr1, curr2;
+
+            //Get the symbol of the selected From & To currencies and save them as strings
+            curr1 = Convert.ToString(fromCurrList.SelectedItem).Substring(0, 3);
+            curr2 = Convert.ToString(toCurrList.SelectedItem).Substring(0, 3);
+
+            //Receive the XML node - root <ExchangeRates> from 31st Dec 2014
+            XmlNode node = myWS2.getExchangeRatesByDate("2014-12-31");
+
+            //Make a list with all <item> nodes from the above node
+            //<ExchangeRates>
+            //  <item>
+            //      ....
+            //  </item>
+            //<ExchangeRates>
+            XmlNodeList nodes = node.SelectNodes("//item");
+
+            foreach (XmlNode nd in nodes)
+            {
+                
+                //1 From Currency / x LTL
+                if (nd["currency"].InnerText == curr1)
+                {
+                    rateFrom = Convert.ToDecimal(nd["rate"].InnerText) / Convert.ToInt32(nd["quantity"].InnerText);
+                }
+
+                //1 To Currency / x LTL
+                if (nd["currency"].InnerText == curr2)
+                {
+                    rateTo = Convert.ToDecimal(nd["rate"].InnerText) / Convert.ToInt32(nd["quantity"].InnerText);
+                }
+            }
+
+            //From Currency / To Currency * amount
+            f = Math.Round(Convert.ToDecimal(amountCurr.Text) * (rateFrom/rateTo), 2);
+            resultCurr.Text = Convert.ToString(f);
+        }
+    }
+}
