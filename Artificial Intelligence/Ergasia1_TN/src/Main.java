4cf7e88adfd230cdc7b1b66c184db0aa11a6190a
ptsionis <tsionispanagiotis@gmail.com>
2023-08-10 20:48:18 +0300

    fixed bfs, added ids

--- Artificial Intelligence/Ergasia1_TN/src/Main.java
+++ Artificial Intelligence/Ergasia1_TN/src/Main.java
@@ -1,8 +1,11 @@
-import java.sql.Time;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.Stack;
+
 import org.apache.commons.math3.special.Gamma;
 
 //Implementation of Knuth's problem
@@ -11,52 +14,93 @@ public class Main {
 	
 	private static Scanner scanner = new Scanner(System.in);
 	private static final int START = 4;
-	static long goal;
 
 	public static void main(String[] args) {
 		
 		System.out.println("Enter a number: ");
-		goal = scanner.nextLong();
+		int goal = scanner.nextInt();
 		
 		System.out.println("Enter search algorithm:");
 		System.out.println("1 = Breadth First Search (BFS)");
 		System.out.println("2 = Iterative Deepening Search (IDS)");
 		int algorithm = scanner.nextInt();
 		
-		State state;
+		State goalState;
 		
 		if (algorithm == 1) {
-			state = breadthFirstSearch(goal);
+			goalState = breadthFirstSearch(goal);
 		} else {
-			//Will be implemented later, for now it gets the same result as above in order to execute
-			state = breadthFirstSearch(goal);
+			goalState = iterativeDeepeningSearch(goal);
 		}
 		
 		System.out.println("Operations needed:");
-		System.out.println(state.getSequence());
+		System.out.println(goalState.getSequence());
 		scanner.close();
 	}
 	
-	public static State breadthFirstSearch(double number) {
+	public static State breadthFirstSearch(int goal) {
 		long startTime = System.currentTimeMillis();
 		Queue<State> stateQueue = new LinkedList<>();
 		stateQueue.add(new State(START, new ArrayList<>(), "start"));
 		
 		while (!stateQueue.isEmpty()) {
 			State currentState = stateQueue.poll();
+			
 			if (currentState.getValue() == goal) {
-				System.out.println("Total time: " + ((System.currentTimeMillis() - startTime) * 0.001) + "s (in ms: " + (System.currentTimeMillis() - startTime) + ")");
+				System.out.println("Total time for BFS: " + ((System.currentTimeMillis() - startTime) * 0.001) + "s (in ms: " + (System.currentTimeMillis() - startTime) + ")");
 				return currentState;
 			}
-				
 			
 			stateQueue.add(new State(Math.sqrt(currentState.getValue()), currentState.getSequence(), "root"));
 			stateQueue.add(new State(Math.floor(currentState.getValue()), currentState.getSequence(), "floor"));
 			stateQueue.add(new State(Gamma.gamma(currentState.getValue() + 1), currentState.getSequence(), "factorial"));
 		}
 		
-		System.out.println("Total time: " + ((System.currentTimeMillis() - startTime) * 0.001) + "s (in ms: " + (System.currentTimeMillis() - startTime) + ")");
+		System.out.println("Total time for BFS: " + ((System.currentTimeMillis() - startTime) * 0.001) + "s (in ms: " + (System.currentTimeMillis() - startTime) + ")");
 		return null;
 	}
+	
+	public static State iterativeDeepeningSearch(int goal) {
+		long startTime = System.currentTimeMillis();
+		
+		State resultState = null;
+		
+		for (int depth = 0; depth < Integer.MAX_VALUE - 1; depth++) {
+			resultState = depthLimitedSearch(goal, depth);
+			if (resultState != null) {
+				System.out.println("Total time for IDS: " + ((System.currentTimeMillis() - startTime) * 0.001) + "s (in ms: " + (System.currentTimeMillis() - startTime) + ")");
+				return resultState;
+			}
+		}
+		
+		System.out.println("Total time for IDS: " + ((System.currentTimeMillis() - startTime) * 0.001) + "s (in ms: " + (System.currentTimeMillis() - startTime) + ")");
+		return resultState;
+	}
+	
+	public static State depthLimitedSearch(int goal, int depth) {
+		Stack<State> stateStack = new Stack<>();
+		stateStack.push(new State(START, new ArrayList<>(), "start"));
+    	
+    	Set<Double> visitedStates = new HashSet<>();
+    	
+    	while (!stateStack.isEmpty()) {
+    		State currentState = stateStack.pop();
+    		
+    		if (currentState.getValue() == goal)
+    			return currentState;
+    		
+    		if (!visitedStates.contains(currentState.getValue())) {
+    			visitedStates.add(currentState.getValue());
+    			
+    			if (currentState.getSequence().size() - 1 <= depth) {
+    				stateStack.push(new State(Math.sqrt(currentState.getValue()), currentState.getSequence(), "root"));
+    				stateStack.push(new State(Math.floor(currentState.getValue()), currentState.getSequence(), "floor"));
+    				stateStack.push(new State(Gamma.gamma(currentState.getValue() + 1), currentState.getSequence(), "factorial"));
+    			}
+    		}
+    	}
+    	
+    	return null;
+    }
 
 }
